8d78310df1ccb6066fb6edbc5827d4099bf76e58
Sarsenov Kuanysh <joy191211@gmail.com>
2024-10-29 11:15:45 +0500

    auth part 1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -3,94 +3,208 @@ from pathlib import Path
 # путь к папке проекта
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# секретный ключ проекта
+SECRET_KEY = "django-insecure-i*dz2*i4)e5i+h$ew_n#)%bku6tc8m4520jakn5b$(-m9=k2_j"
 
-# ключи безопасности
-SECRET_KEY = 'django-insecure-i*dz2*i4)e5i+h$ew_n#)%bku6tc8m4520jakn5b$(-m9=k2_j'
-
-# дебаг режим
+# режим отладки
 DEBUG = True
 
-# список разрешенных хостов
+# список хостов, которые могут обращаться к бэкенду
 ALLOWED_HOSTS = []
 
 
 # подключение приложений
 INSTALLED_APPS = [
-    'django.contrib.admin', # админ панель Django
-    'django.contrib.auth', # базовый модуль аутентификации и управления пользователями
-    'django.contrib.contenttypes', # связь моделей с контентом (например, связь пользователя с его постами)
-    'django.contrib.sessions', # сессии Django (хранение данных о сессии пользователя)
-    'django.contrib.messages', # сообщения пользователю (например, об успешной регистрации)
-    'django.contrib.staticfiles', # обработка статических файлов (CSS, JS, изображения)
+    # админ панель Django
+    "django.contrib.admin",
+    # базовый модуль аутентификации и управления пользователями
+    "django.contrib.auth",
+    # базовый модуль для создания универсальных связей между моделями
+    "django.contrib.contenttypes",
+    # сессии Django (хранение данных о сессии пользователя)
+    "django.contrib.sessions",
+    # модуль для отправки сообщений пользователю (например, об успешной регистрации)
+    "django.contrib.messages",
+    # обработка статических файлов (например, CSS, JS, изображении)
+    "django.contrib.staticfiles",
+    # в письме для подтверждения email будет ссылка на сайт, поэтому нужно подключить sites(нужен для allauth)
+    "django.contrib.sites",
+    # для работы с api (сериализация, десериализация, токен)
+    "rest_framework",
+    # Для создания токенов для аутентификации
+    "rest_framework.authtoken",
+    # базовая функциональность для работы allauth.account
+    "allauth",
+    # для регистрации и подтверждения email
+    "allauth.account",
+    # для регистрации через соцсети(не добавил провайдеров, используется в dj_rest_auth.registration)
+    'allauth.socialaccount',
+    # api для входа и выхода пользователя, сброса пароля и смены пароля
+    "dj_rest_auth",
+    # api для регистрации пользователя (обертка allauth)
+    "dj_rest_auth.registration",
+    # чтобы можно было делать запросы с других доменов, портов (например, с фронтенда на другом порту)
+    "corsheaders",
 ]
 
 # подключение мидлваров для обработки запросов до отправки их во view
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware', # защита от некоторых видов атак
-    'django.contrib.sessions.middleware.SessionMiddleware', # обработка сессий
-    'django.middleware.common.CommonMiddleware', # обработка общих вещей (например, добавление слеша в конце url)
-    'django.middleware.csrf.CsrfViewMiddleware', # защита от CSRF-атак
-    'django.contrib.auth.middleware.AuthenticationMiddleware', # обработка аутентификации
-    'django.contrib.messages.middleware.MessageMiddleware', # обработка сообщений пользователю (например, об успешной регистрации)
-    'django.middleware.clickjacking.XFrameOptionsMiddleware', # защита от кликджекинга
+    # защита от некоторых видов атак
+    "django.middleware.security.SecurityMiddleware",
+    # обработка сессий
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    # обработка общих вещей (например, добавление слеша в конце url)
+    "django.middleware.common.CommonMiddleware",
+    # защита от CSRF-атак
+    "django.middleware.csrf.CsrfViewMiddleware",
+    # обработка аутентификации
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    # обработка сообщений пользователю (например, об успешной регистрации)
+    "django.contrib.messages.middleware.MessageMiddleware",
+    # защита от кликджекинга
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
+    # для работы с cors (разрешение запросов с других доменов, портов)
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
-ROOT_URLCONF = 'backend.urls' # путь к файлу с url-ми
+# для отправки писем на почту
+# бэкенд для отправки писем
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# хост почты
+EMAIL_HOST = "smtp.mail.ru"
+# если используется ssl, то порт 465 (если tls, то 587)
+EMAIL_PORT = 465
+# нельзя использовать и tls и ssl одновременно
+# использовать tls
+EMAIL_USE_TLS = False
+# использовать ssl
+EMAIL_USE_SSL = True
+# почта для отправки писем
+EMAIL_HOST_USER = "alicargo.kz@mail.ru"
+# пароль от почты
+EMAIL_HOST_PASSWORD = "qgFY4xfuCFqi6073Ji5a"
+# почта по умолчанию
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+# бэкенды для аутентификации
+AUTHENTICATION_BACKENDS = (
+    # базовый бэкенд для аутентификации (поиск пользователя в бд)
+    "django.contrib.auth.backends.ModelBackend",
+    # для регистрации и подтверждения email (allauth)
+    "allauth.account.auth_backends.AuthenticationBackend",
+)
+
+# id сайта (нужен для allauth для имени сайта в письме)
+SITE_ID = 1
+# требовать почту при регистрации
+ACCOUNT_EMAIL_REQUIRED = True
+# требовать подтверждение почты
+ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+
+# отключение подтверждения почты
+# если ACCOUNT_EMAIL_VERIFICATION = 'mandatory', то нужно закомментировать эту строку
+# ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+# требовать имя пользователя при регистрации
+ACCOUNT_USERNAME_REQUIRED = True
+# Запоминать сессию пользователя (оставаться в системе после закрытия браузера)
+ACCOUNT_SESSION_REMEMBER = True
+# метод аутентификации (по имени пользователя)
+ACCOUNT_AUTHENTICATION_METHOD = "username"
+# уникальность почты
+ACCOUNT_UNIQUE_EMAIL = True
+
+# определяет способ аутентификации пользователя
+REST_FRAMEWORK = {
+    # все представления будут требовать аутентификации (кроме тех, где указан permission_classes = [AllowAny])
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        # использовать токены для аутентификации
+        "rest_framework.authentication.TokenAuthentication",
+    ]
+}
+
+# путь к файлу с url-ми
+ROOT_URLCONF = "backend.urls"
 
 # шаблоны
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates', # используемый бэкенд для шаблонов (DjangoTemplates)
-        'DIRS': [], # список папок, в которых будут искаться шаблоны
-        'APP_DIRS': True, # указывает, что шаблоны будут искаться в папках приложений
-        'OPTIONS': { # дополнительные настройки
-            'context_processors': [ # список процессоров контекста, которые будут применены к каждому шаблону
-                'django.template.context_processors.debug', # добавляет переменную debug и sql_queries в контекст шаблона (например, для отладки)
-                'django.template.context_processors.request', # добавляет переменную request в контекст шаблона (доступ к данным запроса в шаблоне через request)
-                'django.contrib.auth.context_processors.auth', # добавляет переменные user и perms в контекст шаблона (например, для проверки аутентификации)
-                'django.contrib.messages.context_processors.messages', # добавляет переменную messages в контекст шаблона (например, уведомления об успешной регистрации)
+        # используемый бэкенд для шаблонов (DjangoTemplates)
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        # список папок, в которых будут искаться шаблоны
+        "DIRS": [],
+        # указывает, что шаблоны будут искаться в папках приложений
+        "APP_DIRS": True,
+        # дополнительные настройки
+        "OPTIONS": {
+            # список процессоров контекста, которые будут применены к каждому шаблону
+            "context_processors": [
+                # добавляет переменную debug и sql_queries в контекст шаблона (например, для отладки)
+                "django.template.context_processors.debug",
+                # добавляет переменную request в контекст шаблона (доступ к данным запроса в шаблоне через request)
+                "django.template.context_processors.request",
+                # добавляет переменные user и perms в контекст шаблона (например, для проверки аутентификации)
+                "django.contrib.auth.context_processors.auth",
+                # добавляет переменные messages и DEFAULT_MESSAGE_LEVELS в контекст шаблона (например, для вывода сообщений)
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'backend.wsgi.application' # путь к файлу WSGI-приложения
+# путь к файлу WSGI-приложения
+WSGI_APPLICATION = "backend.wsgi.application"
 
 # база данных
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  # используемый движок базы данных
-        'NAME': BASE_DIR / 'db.sqlite3', # путь к файлу базы данных
+    "default": {
+        # используемый движок базы данных
+        "ENGINE": "django.db.backends.sqlite3",
+        # путь к файлу базы данных
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
-
 
 # валидаторы паролей
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', # проверка на схожесть пароля с атрибутами пользователя
+        # проверка на схожесть пароля с атрибутами пользователя
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', # проверка на минимальную длину пароля
+        # проверка на минимальную длину пароля
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', # проверка на использование общих паролей
+        # проверка на использование общих паролей
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', # проверка на использование цифр в пароле
+        # проверка на использование цифр в пароле
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
+# язык по умолчанию
+LANGUAGE_CODE = "ru"
 
-LANGUAGE_CODE = 'ru' # язык по умолчанию
+# временная зона по умолчанию
+TIME_ZONE = "UTC"
 
-TIME_ZONE = 'UTC' # временная зона по умолчанию
+# использование интернационализации
+USE_I18N = True
 
-USE_I18N = True # использование интернационализации
+# использование временных зон
+USE_TZ = True
 
-USE_TZ = True # использование временных зон
+# URL для статических файлов
+STATIC_URL = "static/"
 
-STATIC_URL = 'static/'
+# тип поля по умолчанию для автоинкремента
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField' # тип поля по умолчанию для автоинкремента
+# настройки для cors, список хостов, которые могут обращаться к бэкенду
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:3000",
+]
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from dj_rest_auth.views import LoginView, LogoutView
 
 urlpatterns = [
+    # подключение админ панели Django
     path('admin/', admin.site.urls),
+    # для регистрации пользователя
+    path('registration/', include('dj_rest_auth.registration.urls')),
+    # для входа
+    path('login/', LoginView.as_view(), name='rest_login'),
+    # для выхода
+    path('logout/', LogoutView.as_view(), name='rest_logout'),
 ]
